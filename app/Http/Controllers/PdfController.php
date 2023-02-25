69e9b0de25a6f24ce1a809af80a88be70ec67110
kokurate <ericksanger66@gmail.com>
2023-02-25 20:21:49 +0800

    successful merge the pdf, but still error when column value is null, so i want to change the file upload logic in the tambah pangkat reguler, when they is not upload a file, i will manually create a file pdf and inside the file is the name of the file with not upload

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -25,37 +25,44 @@ class PdfController extends Controller
 
     public function merge(Request $request, User $user){
         // dd($request);
-        $validator = Validator::make($request->all(),[
-            'kartu_pegawai_nip_baru_bkn' => 'nullable',
-            'sk_cpns' => 'nullable',
-            'sk_pangkat_terakhir' => 'nullable',
-        ]);
+    //     $validator = Validator::make($request->all(),[
+    //         'kartu_pegawai_nip_baru_bkn' => 'nullable',
+    //         'sk_cpns' => 'nullable',
+    //         'sk_pangkat_terakhir' => 'nullable',
+    //     ]);
 
-    // Validasi
-    $validatedData = $validator->validated();
+    // // Validasi
+    // $validatedData = $validator->validated();
     $oMerger = PDFMerger::init();
     // dd($validatedData);
     
-    $oMerger->addPDF(storage_path('app/public/' . substr($validatedData['kartu_pegawai_nip_baru_bkn'], strlen(asset('storage/')))),'all');
-    $oMerger->addPDF(storage_path('app/public/' . substr($validatedData['sk_cpns'], strlen(asset('storage/')))),'all');
-    $oMerger->addPDF(storage_path('app/public/' . substr($validatedData['sk_pangkat_terakhir'], strlen(asset('storage/')))),'all');
-    // $oMerger->addPDF(storage_path('app/public/' . $validatedData['kartu_pegawai_nip_baru_bkn']), 'all');
-    // $oMerger->addPDF(storage_path('app/public/' . $validatedData['sk_cpns']), 'all');
-    // $oMerger->addPDF(storage_path('app/public/' . $validatedData['sk_pangkat_terakhir']), 'all');
+    // $oMerger->addPDF(storage_path('app/public/' . substr($validatedData['kartu_pegawai_nip_baru_bkn'], strlen(asset('storage/')))),'all');
+    // $oMerger->addPDF(storage_path('app/public/' . substr($validatedData['sk_cpns'], strlen(asset('storage/')))),'all');
+    // $oMerger->addPDF(storage_path('app/public/' . substr($validatedData['sk_pangkat_terakhir'], strlen(asset('storage/')))),'all');
+    
+    User::where('id', $user->id)->update(['status' => 'Disetujui']);
+
+    $oMerger->addPDF(storage_path('app/public/' . substr($request->kartu_pegawai_nip_baru_bkn, strlen(asset('storage/')))),'all');
+    $oMerger->addPDF(storage_path('app/public/' . substr($request->sk_cpns, strlen(asset('storage/')))),'all');
+    $oMerger->addPDF(storage_path('app/public/' . substr($request->sk_pangkat_terakhir, strlen(asset('storage/')))),'all');
+    $oMerger->addPDF(storage_path('app/public/' . substr($request->sk_jabfung_terakhir_dan_pak, strlen(asset('storage/')))),'all');
+    $oMerger->addPDF(storage_path('app/public/' . substr($request->ppk_dan_skp, strlen(asset('storage/')))),'all');
+    $oMerger->addPDF(storage_path('app/public/' . substr($request->ijazah_terakhir, strlen(asset('storage/')))),'all');
+    $oMerger->addPDF(storage_path('app/public/' . substr($request->sk_tugas_belajar_atau_surat_izin_studi, strlen(asset('storage/')))),'all');
+    $oMerger->addPDF(storage_path('app/public/' . substr($request->keterangan_membina_mata_kuliah_dari_jurusan, strlen(asset('storage/')))),'all');
+    $oMerger->addPDF(storage_path('app/public/' . substr($request->surat_pernyataan_setiap_bidang_tridharma, strlen(asset('storage/')))),'all');
+
 
 
     $namadosen = $user->name;
     $slug = Str::slug($namadosen);
 
-
-   
     $fileName = '/storage/dosen/'.$slug.'/'. time().'-merge'.'.pdf';
-    // $fileName = 'merge/'. time().'-merge'.'.pdf';
 
     $oMerger->merge();
     $oMerger->save(public_path($fileName));
 
-    // return response()->download(public_path('merged_result.pdf'));
+    return response()->download(public_path($fileName));
 
     }
 }

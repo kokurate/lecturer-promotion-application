6b39342df2de30ffa9d4f94f7acf093cafd3393c
kokurate <ericksanger66@gmail.com>
2023-03-09 12:28:50 +0800

    modifed the fakultas name

--- database/seeders/FakultasSeeder.php
+++ database/seeders/FakultasSeeder.php
@@ -15,11 +15,11 @@ class FakultasSeeder extends Seeder
     public function run()
     {
         fakultas::create(['nama' => 'Fakultas Ilmu Pendidikan']); //1 
-        fakultas::create(['nama' => 'Fakultas Matematika Dan Ilmu Pengetahuan Alam']); // 2
+        fakultas::create(['nama' => 'Fakultas Matematika Ilmu Pengetahuan Alam Dan Kebumian']); // 2
         fakultas::create(['nama' => 'Fakultas Ilmu Keolahragaan']); // 3
         fakultas::create(['nama' => 'Fakultas Teknik']); // 4
         fakultas::create(['nama' => 'Fakultas Ekonomi']); // 5
-        fakultas::create(['nama' => 'Fakultas Ilmu Sosial']); // 6
+        fakultas::create(['nama' => 'Fakultas Ilmu Sosial Dan Hukum']); // 6
         fakultas::create(['nama' => 'Fakultas Bahasa Dan Seni']); // 7
 
     }
